3f8591d51ed9a27d416effe094e2c1e810e97804
petermwash <petermwash48@gmail.com>
2018-10-16 08:42:12 +0300

    [Chore] Setup the project

---
+++ app/src/main/java/pemwa/com/javadevsnairobi/MainActivity.java
@@ -0,0 +1,13 @@
+package pemwa.com.javadevsnairobi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
