862924809a334f288f88bdf96ade62e6e11de221
Peter <petermwash48@gmail.com>
2018-10-18 10:31:28 +0300

    #155768857 Connect recycler view to the main activity. (#6)
    * [Chore #155738450] Integrate Circle CI
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    [Chore #155738450] Integrate Code climate
    
    * [Feature #155768857] Connect the recyclerView layout to the MainActivity

--- app/src/main/java/pemwa/com/javadevsnairobi/activity/MainActivity.java
+++ app/src/main/java/pemwa/com/javadevsnairobi/activity/MainActivity.java
@@ -2,14 +2,24 @@ package pemwa.com.javadevsnairobi.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import pemwa.com.javadevsnairobi.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        recyclerView = findViewById(R.id.customRecyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(null);
     }
 }
