1cefc237d1bb88b4021e8d58fde2af23143e4cac
petermwash <petermwash48@gmail.com>
2018-10-22 06:08:32 -0700

    [Feature #155890684] Create a retrofit instance

---
+++ app/src/main/java/pemwa/com/javadevsnairobi/service/GithubService.java
@@ -0,0 +1,26 @@
+package pemwa.com.javadevsnairobi.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class GithubService {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://api.github.com/";
+
+
+    public GithubAPI getGithubAPI() {
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit.create(GithubAPI.class);
+    }
+}
